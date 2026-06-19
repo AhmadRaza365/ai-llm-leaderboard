65ff718d246d65d9fd014294921cf454cbe5917b
Ahmad Raza <ahmadrazabhatti30@gmail.com>
2026-06-20 00:16:48 +0500

    Adding ads tag

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
         height={4}
       />
       {children}
+
+
+      <script src="https://quge5.com/88/tag.min.js" data-zone="251637" async data-cfasync="false"></script>
       <Footer />
     </>
   )
